42d236b01d3ac76a66476f38d7eaad5bf5000543
Jackie Lai <jlai@bbbsci.org>
2021-07-10 20:04:45 -0400

    (Jackie) Updated signup/login functionality to work with server side but receiving a 500 internal error from the server when testing.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 import Navbar from './components/navigation/Navbar';
 import Portal from './components/auth/Portal';
 
 function App() {
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'));
+    }
+  }, [])
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  }
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('');
+  }
+  
   return (
     <div className="App">
-      <Navbar />
-      <Portal />
+      <Navbar clickLogout={clearToken} />
+      <Portal updateToken={updateToken} />
     </div>
   );
 }
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -9,14 +9,15 @@ import {
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
-const Navbar = () => {
+const Navbar = (props) => {
     const useStyles = makeStyles((theme) => ({
         root: {
             flexGrow: 1,
             width: '100vw',
+            maxHeight: '10vh',
         },
         appBar: {
-            backgroundColor: 'rgba(230,1,10,0.9)'
+            backgroundColor: 'rgba(230,1,10,0.9)',
         },
         menuButton: {
             marginRight: theme.spacing(2),
@@ -30,7 +31,7 @@ const Navbar = () => {
 
     return (
         <div className={classes.root}>
-            <AppBar className={classes.appBar} position="static">
+            <AppBar className={classes.appBar} position="fixed">
                 <Toolbar>
                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
@@ -38,7 +39,7 @@ const Navbar = () => {
                     <Typography variant="h6" className={classes.title}>
                         DnD NPC Database
                     </Typography>
-                    <Button color="inherit">Logout</Button>
+                    <Button color="inherit" onClick={props.clickLogout}>Logout</Button>
                 </Toolbar>
             </AppBar>
         </div>
