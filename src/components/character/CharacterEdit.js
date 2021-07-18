2832c1a73a1fc01c7396e89fe1556837cfa54150
Sarah Tanner <sktanner92@gmail.com>
2021-07-18 15:23:24 -0400

    Finished character update and added races

--- src/components/character/CharacterEdit.js
+++ src/components/character/CharacterEdit.js
@@ -1,6 +1,5 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody } from 'reactstrap'
-//import material ui form
 
 const CharacterEdit = (props) => {
     const [editName, setEditName] = useState(props.characterToUpdate.name)
@@ -16,47 +15,97 @@ const CharacterEdit = (props) => {
         event.preventDefault()
         fetch(`http://localhost:3000/character/${props.characterToUpdate.id}`, {
             method: 'PUT',
-            body: JSON.stringify({log: {
-                name: editName,
-                location: editLocation,
-                race: editRace,
-                gender: editGender,
-                age: editAge,
-                alignment: editAlignment,
-                profession: editProfession,
-                trait: editTrait
-            }}),
+            body: JSON.stringify({
+                character: {
+                    name: editName,
+                    location: editLocation,
+                    race: editRace,
+                    gender: editGender,
+                    age: editAge,
+                    alignment: editAlignment,
+                    profession: editProfession,
+                    trait: editTrait
+                }
+            }),
             headers: new Headers({
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${props.token}`
             })
-        }) .then((res) => {
+        }).then((res) => {
             props.fetchCharacters()
             props.updateOff()
         })
     }
 
-    return(
+    return (
         <Modal isOpen={true}>
             <ModalHeader>Edit a Character</ModalHeader>
             <ModalBody>
                 <Form onSubmit={CharacterUpdate}>
                     <FormGroup>
                         <Label htmlFor="name">Edit Name:</Label>
-                        <Input name="name" value={editName} onChange={(e) => setEditName(e.target.value)}/>
+                        <Input name="name" value={editName} onChange={(e) => setEditName(e.target.value)} />
                     </FormGroup>
                     <FormGroup>
                         <Label htmlFor="location">Edit Location:</Label>
-                        <Input name="location" value={editLocation} onChange={(e) => setEditLocation(e.target.value)}/>
+                        <Input name="location" value={editLocation} onChange={(e) => setEditLocation(e.target.value)} />
                     </FormGroup>
                     <FormGroup>
                         <Label htmlFor="race">Edit Race:</Label>
-                        <Input name="race" value={editRace} onChange={(e) => setEditRace(e.target.value)}>
+                        <Input type="select" name="race" value={editRace} onChange={(e) => setEditRace(e.target.value)}>
                             <option></option>
-                            <option value="Elf">Elf</option>
+                            <option value="Dragonborn">Dragonborn</option>
                             <option value="Dwarf">Dwarf</option>
+                            <option value="Elf">Elf</option>
+                            <option value="Gnome">Gnome</option>
+                            <option value="Half-Elf">Half-Elf</option>
+                            <option value="Halfling">Halfling</option>
+                            <option value="Half-Orc">Half-Orc</option>
                             <option value="Human">Human</option>
+                            <option value="Tiefling">Tiefling</option>
                         </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="gender">Edit Gender:</Label>
+                        <Input type="select" name="gender" value={editGender} onChange={(e) => setEditGender(e.target.value)}>
+                            <option></option>
+                            <option value="Female">Female</option>
+                            <option value="Male">Male</option>
+                            <option value="Nonbinary">Nonbinary</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="age">Edit Age:</Label>
+                        <Input type="select" name="age" value={editAge} onChange={(e) => setEditAge(e.target.value)}>
+                            <option></option>
+                            <option value="Child">Child</option>
+                            <option value="Young Adult">Young Adult</option>
+                            <option value="Adult">Adult</option>
+                            <option value="Elder">Elder</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="alignment">Edit Alignment:</Label>
+                        <Input type="select" name="alignment" value={editAlignment} onChange={(e) => setEditAlignment(e.target.value)}>
+                            <option></option>
+                            <option value="Lawful Good">Lawful Good</option>
+                            <option value="Neutral Good">Neutral Good</option>
+                            <option value="Chaotic Good">Chaotic Good</option>
+                            <option value="Lawful Neutral">Lawful Neutral</option>
+                            <option value="True Neutral">True Neutral</option>
+                            <option value="Chaotic Neutral">Chaotic Neutral</option>
+                            <option value="Lawful Evil">Lawful Evil</option>
+                            <option value="Neutral Evil">Neutral Evil</option>
+                            <option value="Chaotic Evil">Chaotic Evil</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="profession">Edit Profession:</Label>
+                            <Input name="profession" placeholder="Profession" value={editProfession} onChange={(e) => setEditProfession(e.target.value)} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="trait">Edit Trait:</Label>
+                            <Input name="trait" placeholder="Trait" value={editTrait} onChange={(e) => setEditTrait(e.target.value)} />
                     </FormGroup>
                     <Button type="submit">Update the character!</Button>
                 </Form>
