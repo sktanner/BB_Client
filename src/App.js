8ba5a45e16eaf20e10b9fce6596ddad11e8bd729
Jackie Lai <jlai@bbbsci.org>
2021-07-19 18:55:41 -0400

    (Jackie) Placed Card.js in CharacterIndex.js file so that cards do not appear on login/signup page.

--- src/App.js
+++ src/App.js
@@ -3,27 +3,14 @@ import { useEffect, useState } from 'react';
 
 import Navbar from './components/navigation/Navbar';
 import Portal from './components/auth/Portal';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
 
 // import CharacterTable from './components/character/Table';
 //0f7756f76f26540951a6290c223f4e0ecc7113ed
 import CharacterIndex from './components/character/CharacterIndex';
 // import CharacterTable from './components/character/CharacterTable';
-import CharacterCard from './components/character/Card';
-
-const useStyles = makeStyles({
-  gridContainer: {
-    paddingLeft:'20px',
-    paddingRight: '20px',
-    paddingBottom: '20px'
-  }
-});
-
 
 
 function App() {
-  const classes = useStyles();
   const [sessionToken, setSessionToken] = useState('');
 
   useEffect(() => {
@@ -52,21 +39,6 @@ function App() {
     <div className="App">
       <Navbar clickLogout={clearToken} token={sessionToken} />
       {protectedView()}
-     
-      <Grid container spacing={4} className={classes.gridContainer} >
-        <Grid item xs={12} sm={6} md={4}>
-          <CharacterCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <CharacterCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <CharacterCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <CharacterCard />
-        </Grid>
-      </Grid>
 
     </div>
   );
--- src/components/character/CharacterIndex.js
+++ src/components/character/CharacterIndex.js
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from 'react'
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
 import CharacterTable from './CharacterTable'
 import CharacterEdit from './CharacterEdit'
 import CharacterCreate from './CharacterCreate'
+import CharacterCard from './Card'
 
 const CharacterIndex = (props) => {
+    const useStyles = makeStyles({
+        gridContainer: {
+            paddingLeft: '20px',
+            paddingRight: '20px',
+            paddingBottom: '20px'
+        }
+    });
+
+    const classes = useStyles();
+
     const [character, setCharacter] = useState([])
     const [updateActive, setUpdateActive] = useState(false)
     const [characterToUpdate, setCharacterToUpdate] = useState([])
@@ -44,8 +58,26 @@ const CharacterIndex = (props) => {
             <CharacterCreate fetchCharacters={fetchCharacters} token={props.token} />
 
             <CharacterTable character={character} editUpdateCharacter={editUpdateCharacter} updateOn={updateOn} fetchCharacters={fetchCharacters} token={props.token} />
-            
+
             {updateActive ? <CharacterEdit characterToUpdate={characterToUpdate} updateOff={updateOff} token={props.token} fetchCharacters={fetchCharacters} /> : <></>}
+
+            <br />
+            
+            <Grid container spacing={4} className={classes.gridContainer} >
+                <Grid item xs={12} sm={6} md={4}>
+                    <CharacterCard />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <CharacterCard />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <CharacterCard />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <CharacterCard />
+                </Grid>
+            </Grid>
+
         </div>
     )
 }
