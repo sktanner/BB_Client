6c88ecf8bed4646efc12845cdba5bdbb972f1867
Sarah Tanner <sktanner92@gmail.com>
2021-07-21 23:45:26 -0400

    (Sarah) Added random options and professions

--- src/components/character/CharacterCreate.js
+++ src/components/character/CharacterCreate.js
@@ -33,6 +33,24 @@ const CharacterCreate = (props) => {
     const [alignment, setAlignment] = useState('')
     const [profession, setProfession] = useState('')
     const [trait, setTrait] = useState('')
+
+    let randomRaces = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Half-Orc', 'Human', 'Tiefling']
+    let randomRace = randomRaces[Math.floor(Math.random()*randomRaces.length)]
+    // console.log(randomRace)
+
+    let randomGenders = ['Female', 'Male', 'Nonbinary']
+    let randomGender = randomGenders[Math.floor(Math.random()*randomGenders.length)]
+    // console.log(randomGender)
+
+    let randomAges = ['Child', 'Young Adult', 'Adult', 'Elder']
+    let randomAge = randomAges[Math.floor(Math.random()*randomAges.length)]
+
+    let randomAlignments = ['Lawful Good', 'Neutral Good', 'Chaotic Good', 'Lawful Neutral', 'True Neutral', 'Chaotic Neutral', 'Lawful Evil', 'Neutral Evil', 'Chaotic Evil']
+    let randomAlignment = randomAlignments[Math.floor(Math.random()*randomAlignments.length)]
+
+    let randomProfessions = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard', 'Acolyte', 'Baker', 'Bandit', 'Beggar', 'Blacksmith', 'Bounty Hunter', 'Cook', 'Entertainer', 'Explorer', 'Farmer', 'Fisherman', 'Gravedigger', 'Hermit', 'Historian', 'Innkeeper', 'Knight', 'Medic', 'Merchant', 'Musician', 'Sailor', 'Smuggler', 'Soldier', 'Squire']
+    let randomProfession = randomProfessions[Math.floor(Math.random()*randomProfessions.length)]
+
     //Randomizer props
     // const [randRace, setRandRace] = useState('')
     // const [randGender, setRandGender] = useState('')
@@ -116,7 +134,8 @@ const CharacterCreate = (props) => {
                     <Label htmlFor="race" sm={4}>Race:</Label>
                     <Col sm={8}>
                         <Input type="select" name="race" value={race} onChange={(e) => setRace(e.target.value)}>
-                            <option></option>
+                            <option value="" selected disabled>Please select</option>
+                            <option value={randomRace}>Random</option>
                             <option value="Dragonborn">Dragonborn</option>
                             <option value="Dwarf">Dwarf</option>
                             <option value="Elf">Elf</option>
@@ -133,7 +152,8 @@ const CharacterCreate = (props) => {
                     <Label htmlFor="gender" sm={4}>Gender:</Label>
                     <Col sm={8}>
                         <Input type="select" name="gender" value={gender} onChange={(e) => setGender(e.target.value)}>
-                            <option></option>
+                            <option value="" selected disabled>Please select</option>
+                            <option value={randomGender}>Random</option>
                             <option value="Female">Female</option>
                             <option value="Male">Male</option>
                             <option value="Nonbinary">Nonbinary</option>
@@ -144,7 +164,8 @@ const CharacterCreate = (props) => {
                     <Label htmlFor="age" sm={4}>Age:</Label>
                     <Col sm={8}>
                         <Input type="select" name="age" value={age} onChange={(e) => setAge(e.target.value)}>
-                            <option></option>
+                            <option value="" selected disabled>Please select</option>
+                            <option value={randomAge}>Random</option>
                             <option value="Child">Child</option>
                             <option value="Young Adult">Young Adult</option>
                             <option value="Adult">Adult</option>
@@ -156,7 +177,8 @@ const CharacterCreate = (props) => {
                     <Label htmlFor="alignment" sm={4}>Alignment:</Label>
                     <Col sm={8}>
                         <Input type="select" name="alignment" value={alignment} onChange={(e) => setAlignment(e.target.value)}>
-                            <option></option>
+                            <option value="" selected disabled>Please select</option>
+                            <option value={randomAlignment}>Random</option>
                             <option value="Lawful Good">Lawful Good</option>
                             <option value="Neutral Good">Neutral Good</option>
                             <option value="Chaotic Good">Chaotic Good</option>
@@ -172,7 +194,45 @@ const CharacterCreate = (props) => {
                 <FormGroup row>
                     <Label htmlFor="profession" sm={4}>Profession:</Label>
                     <Col sm={8}>
-                        <Input name="profession" placeholder="Profession" value={profession} onChange={(e) => setProfession(e.target.value)} />
+                        <Input type="select" name="profession" value={profession} onChange={(e) => setProfession(e.target.value)}>
+                            <option value="" selected disabled>Please select</option>
+                            <option value={randomProfession}>Random</option>
+                            <option value='Barbarian'>Barbarian</option>
+                            <option value='Bard'>Bard</option>
+                            <option value='Cleric'>Cleric</option>
+                            <option value='Druid'>Druid</option>
+                            <option value='Fighter'>Fighter</option>
+                            <option value='Monk'>Monk</option>
+                            <option value='Paladin'>Paladin</option>
+                            <option value='Ranger'>Ranger</option>
+                            <option value='Rogue'>Rogue</option>
+                            <option value='Sorcerer'>Sorcerer</option>
+                            <option value='Warlock'>Warlock</option>
+                            <option value='Wizard'>Wizard</option>
+                            <option value='Acolyte'>Acolyte</option>
+                            <option value='Baker'>Baker</option>
+                            <option value='Bandit'>Bandit</option>
+                            <option value='Beggar'>Beggar</option>
+                            <option value='Blacksmith'>Blacksmith</option>
+                            <option value='Bounty Hunter'>Bounty Hunter</option>
+                            <option value='Cook'>Cook</option>
+                            <option value='Entertainer'>Entertainer</option>
+                            <option value='Explorer'>Explorer</option>
+                            <option value='Farmer'>Farmer</option>
+                            <option value='Fisherman'>Fisherman</option>
+                            <option value='Gravedigger'>Gravedigger</option>
+                            <option value='Hermit'>Hermit</option>
+                            <option value='Historian'>Historian</option>
+                            <option value='Innkeeper'>Innkeeper</option>
+                            <option value='Knight'>Knight</option>
+                            <option value='Medic'>Medic</option>
+                            <option value='Merchant'>Merchant</option>
+                            <option value='Musician'>Musician</option>
+                            <option value='Sailor'>Sailor</option>
+                            <option value='Smuggler'>Smuggler</option>
+                            <option value='Soldier'>Soldier</option>
+                            <option value='Squire'>Squire</option>
+                        </Input>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
