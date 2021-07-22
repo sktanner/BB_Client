3f58b940e05acdc8eb55a4068111729dbb5de6ad
Jackie Lai <jlai@bbbsci.org>
2021-07-21 22:54:33 -0400

    (Jackie) Made minor design edits plus created logic for logout button to appear (and not appear) when necessary.

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -17,13 +17,19 @@ const Navbar = (props) => {
             maxHeight: '10vh',
         },
         appBar: {
-            backgroundColor: 'rgba(230,1,10,0.9)',
+            backgroundColor: 'rgba(230,1,10,0.8)',
         },
         menuButton: {
             marginRight: theme.spacing(2),
         },
         title: {
             flexGrow: 1,
+            color: "black",
+            fontWeight: "bold",
+        },
+        logoutButton: {
+            color: "black",
+            fontWeight: "bold",
         },
     }));
 
@@ -33,13 +39,14 @@ const Navbar = (props) => {
         <div className={classes.root}>
             <AppBar className={classes.appBar} position="fixed">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <img width="75em" src="./assets/dnd_bug_1c_blk_v1_xl_rgb.png" />
+                    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
+                    </IconButton> */}
+                    <Typography variant="h5" className={classes.title}>
                         DnD NPC Database
                     </Typography>
-                    <Button color="inherit" onClick={props.clickLogout}>Logout</Button>
+                    { props.token ? <Button className={classes.logoutButton} color="inherit" onClick={props.clickLogout}>Logout</Button> : null }
                 </Toolbar>
             </AppBar>
         </div>
